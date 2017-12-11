2749923fcf1b54f2673a56de718a5b74ceeadb99
rkrk117 <32203360+rkrk117@users.noreply.github.com>
2017-12-11 10:36:57 +0500

    Add files via upload

--- client-core.js
+++ client-core.js
@@ -1,11 +1,65 @@
 'use strict';
 
 module.exports.execute = execute;
-module.exports.isStar = true;
+
+const chalk = require('chalk');
+const request = require('request');
+
+const red = chalk.hex('#f00');
+const green = chalk.hex('#0f0');
+
+function addParameter(res, name, args) {
+    for (let i in args) {
+        if (args[i] === name) {
+            return args[i + 1];
+        }
+        if (args[i].indexOf(name) !== -1) {
+            return args[i].slice(args[i].indexOf('=') + 1);
+        }
+    }
+}
+
+function parseArgs(args) {
+    var result = {};
+    result.from = addParameter(result, '--from', args);
+    result.to = addParameter(result, '--to', args);
+    result.text = addParameter(result, '--text', args);
+
+    return result;
+}
+
+function performOutput(message) {
+    let result = '';
+    if (message.from) {
+        result += (`${red('FROM')}: ${message.from}\n`);
+    }
+    if (message.to) {
+        result += (`${red('TO')}: ${message.to}\n`);
+    }
+    result += (`${green('TEXT')}: ${message.text}`);
+
+    return result;
+}
 
 function execute() {
-    // Внутри этой функции нужно получить и обработать аргументы командной строки
-    const args = process.argv;
+    let opts = parseArgs(process.argv);
+    function requestPromise({ qs = {}, method = 'GET', json = true }) {
+        return new Promise((resolve, reject) => {
+            request({ baseUrl: 'http://localhost:8080/messages/', url: '/', qs, method, json },
+                (err, response, body) => err ? reject(err) : resolve(body));
+        });
+    }
 
-    return Promise.resolve('Это строка будет выведена на консоль');
+    switch (process.argv[2]) {
+        case 'list':
+            return requestPromise({ qs: { from: opts.from, to: opts.to } })
+                .then(messages => messages.map(x => performOutput(x)))
+                .then(messages => messages.join('\n\n'));
+        case 'send':
+            return requestPromise({ qs: { from: opts.from, to: opts.to },
+                method: 'POST', json: { text: opts.text } })
+                .then(x => performOutput(x));
+        default:
+            return Promise.reject();
+    }
 }
--- server-core.js
+++ server-core.js
@@ -4,9 +4,55 @@ const http = require('http');
 
 const server = http.createServer();
 
+const urlapi = require('url');
+const queryapi = require('querystring');
+
+let vault = [];
+
+function extractQuery(from, to) {
+    let filter;
+    if (from && to) {
+        filter = (note) => note.from === from && note.to === to;
+    } else if (from) {
+        filter = (note) => note.from === from;
+    } else if (to) {
+        filter = (note) => note.to === to;
+    } else {
+        filter = () => true;
+    }
+
+    return vault.filter(filter);
+}
+
 server.on('request', (req, res) => {
-    // Тут нужно обработать запрос
-    res.end();
+    let { query } = urlapi.parse(req.url);
+    let { from, to } = queryapi.parse(query);
+    res.setHeader('content-type', 'application/json');
+    if (req.url.match(/^\/messages/) && req.method === 'GET') {
+        res.write(JSON.stringify(extractQuery(from, to)));
+        res.end();
+    } else if (req.url.match(/^\/messages/) && req.method === 'POST') {
+        let note = {};
+        let text = '';
+        req.on('data', more => {
+            text += more;
+        });
+        req.on('end', () => {
+            if (from) {
+                note.from = from;
+            }
+            if (to) {
+                note.to = to;
+            }
+            note.text = JSON.parse(text).text;
+            vault.push(note);
+            res.write(JSON.stringify(note));
+            res.end();
+        });
+    } else {
+        res.statusCode = 404;
+        res.end();
+    }
 });
 
 module.exports = server;
